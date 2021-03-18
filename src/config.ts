26b02e40bc7fcf71d2d1eb20f58ed66a1c8f2cc2
Serubin <Solomon@fossa.com>
2021-03-18 12:25:17 -0700

    Remove uncessary logic

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@ import { getInput } from '@actions/core';
 
 export const FOSSA_API_KEY = getInput('api-key', {required: true});
 export const CONTAINER = getInput('container', {required: false});
-export const RUN_TESTS = getInput('run-tests', {required: false}).toLocaleLowerCase() === 'true' || false;
+export const RUN_TESTS = getInput('run-tests', {required: false}).toLocaleLowerCase() === 'true';
