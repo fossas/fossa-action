22dd6fa06cf418d6b932a4619baf2da063dd3e21
himynameisdave <dave@fossa.com>
2022-03-11 10:20:38 -0800

    🌳 Add --branch option

--- src/config.ts
+++ src/config.ts
@@ -4,3 +4,4 @@ export const FOSSA_API_KEY = getInput('api-key', {required: true});
 export const CONTAINER = getInput('container', {required: false});
 export const RUN_TESTS = getInput('run-tests', {required: false}).toLocaleLowerCase() === 'true';
 export const ENDPOINT = getInput('endpoint', {required: false});
+export const BRANCH = getInput('branch', {required: false});
